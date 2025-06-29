1c6be17a48693e7adbbb56e9c5314dafbda0166f
Emanoel Portugal <71513403+eportugal@users.noreply.github.com>
2025-06-29 15:54:57 -0300

    adjusting amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,7 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource.js";
-import { data } from "./data/resource.js";
 
 // Deixe o backend.ts simples como antes
 export const backend = defineBackend({
   auth,
-  data,
-  askMistral,
 });
